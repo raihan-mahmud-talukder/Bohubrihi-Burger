ff8eb84f030080660b977d8658de7eea8e351ee1
Raihan Mahmud <raihan_uits@outlook.com>
2021-11-30 16:54:02 +0600

    Initial Setup

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <p>Header</p>
+        </div>
+    )
+}
+
+export default Header;
