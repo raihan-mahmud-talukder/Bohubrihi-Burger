462232ca9f77bd41079132f4c926454b80115038
Raihan Mahmud <raihan_uits@outlook.com>
2021-11-30 18:26:42 +0600

    Navigation Bar

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,25 @@
-import { getDefaultNormalizer } from "@testing-library/react";
 import React from "react";
+import './Header.css';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import Logo from '../../assets/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            <p>Header</p>
+        <div className='Navigation'>
+            <Navbar style={{
+                backgroundColor: '#d70f64', height: '70px'
+            }}>
+                <NavbarBrand href='/' className= 'mr-auto ml-md-5 Brand'>
+                    <img src={Logo} alt='Logo' width='80px' />
+                </NavbarBrand>
+                <Nav className='mr-md-5'>
+                    <NavItem>
+                        <NavLink href='#' className='NavLink'>
+                            Something
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
