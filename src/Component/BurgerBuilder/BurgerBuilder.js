88c107ff8c34475b96eb7cb2f882c8fdcb731e32
Raihan Mahmud <raihan_uits@outlook.com>
2021-12-06 06:26:56 +0600

    Showing Price

--- src/Component/BurgerBuilder/BurgerBuilder.js
+++ src/Component/BurgerBuilder/BurgerBuilder.js
@@ -2,32 +2,41 @@ import React, { Component } from "react";
 import Burger from "./Burger/Burger";
 import Controls from './Controls/Controls';
 
+const INGREDIENT_PRICES = {
+    salad: 20,
+    cheese: 40,
+    meat: 90
+}
+
 export default class BurgerBuilder extends Component {
     state = {
         ingredients: [
             { type: 'salad', amount: 0 },
             { type: 'cheese', amount: 0 },
             { type: 'meat', amount: 0 }
-        ]
+        ],
+        totalPrice: 80,
     }
 
     addIngredient = type => {
         const ingredients = [...this.state.ingredients];
+        const new_price = this.state.totalPrice + INGREDIENT_PRICES[type];
         for (let item of ingredients) {
             if (item.type === type) item.amount++;
         }
-        this.setState({ ingredients: ingredients });
+        this.setState({ ingredients: ingredients, totalPrice: new_price });
     }
 
     removeIngredient = type => {
         const ingredients = [...this.state.ingredients];
+        const new_price = this.state.totalPrice - INGREDIENT_PRICES[type];
         for (let item of ingredients) {
             if (item.type === type) {
                 if (item.amount <= 0) return;
                 item.amount--;
             }
         }
-        this.setState({ ingredients: ingredients });
+        this.setState({ ingredients: ingredients, totalPrice: new_price });
         console.log(type);
     }
 
@@ -37,7 +46,8 @@ export default class BurgerBuilder extends Component {
                 <Burger ingredients={this.state.ingredients} />
                 <Controls
                     ingredientAdded={this.addIngredient}
-                    ingredientRemoved={this.removeIngredient} />
+                    ingredientRemoved={this.removeIngredient}
+                    price={this.state.totalPrice} />
             </div>
         )
     }
