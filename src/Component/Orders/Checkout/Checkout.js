b2fcc4a04f4146197a25133bad50597537dd3633
Raihan Mahmud <raihan_uits@outlook.com>
2021-12-19 19:34:15 +0600

    Improving Checkout

--- src/Component/Orders/Checkout/Checkout.js
+++ src/Component/Orders/Checkout/Checkout.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { Component } from "react";
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalBody } from 'reactstrap';
 import { useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from "axios";
 import Spinner from "../../Spinner/Spinner";
+import { resetIngredients } from "../../../redux/actionCreator";
 
 const mapStateToProps = state => {
     return {
         ingredients: state.ingredients,
         totalPrice: state.totalPrice,
         purchasable: state.purchasable,
+    }
+}
+
+const mapDispachToProps = dispatch => {
+    return {
+        resetIngredients: () => dispatch(resetIngredients()),
     }
 }
 
@@ -22,6 +29,8 @@ class Checkout extends Component {
             paymentType: 'Cash On Delivery',
         },
         isLoading: false,
+        isModalOpen: false,
+        modalMSG: '',
     }
 
     goBack = () => {
@@ -46,16 +55,29 @@ class Checkout extends Component {
             orderTime: new Date(),
         }
         axios.post('https://burger-builder-480c2-default-rtdb.firebaseio.com/orders.json', order)
-        .then(response => {
-            if (response.status === 200) {
-                this.setState({ isLoading: false })
-            } else {
-                this.setState({ isLoading: false })
-            }
-        })
-        .catch(err => {
-            this.setState({ isLoading: false })
-        })
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({
+                        isLoading: false,
+                        isModalOpen: true,
+                        modalMSG: 'Order Placed Successfully!',
+                    })
+                    this.props.resetIngredients();
+                } else {
+                    this.setState({
+                        isLoading: false,
+                        isModalOpen: true,
+                        modalMSG: 'Something Went Wrong! Order Again',
+                    })
+                }
+            })
+            .catch(err => {
+                this.setState({
+                    isLoading: false,
+                    isModalOpen: true,
+                    modalMSG: 'Something Went Wrong! Order Again',
+                })
+            })
     }
 
     render() {
@@ -87,6 +109,11 @@ class Checkout extends Component {
         return (
             <div>
                 {this.state.isLoading ? <Spinner /> : form}
+                <Modal isOpen={this.state.isModalOpen} onClick={this.goBack}>
+                    <ModalBody>
+                        <p>{this.state.modalMSG}</p>
+                    </ModalBody>
+                </Modal>
             </div>
         )
     }
@@ -97,4 +124,4 @@ function WithNavigate(props) {
     return <Checkout {...props} navigate={navigate} />
 }
 
-export default connect(mapStateToProps)(WithNavigate);
+export default connect(mapStateToProps, mapDispachToProps)(WithNavigate);
