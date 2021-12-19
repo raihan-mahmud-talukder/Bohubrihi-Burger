a8425c1aff9dcb38489ee58ecca8073ba5ef7ac2
Raihan Mahmud <raihan_uits@outlook.com>
2021-12-19 13:19:54 +0600

    Create Checkout

--- src/Component/Orders/Checkout/Checkout.js
+++ src/Component/Orders/Checkout/Checkout.js
@@ -1,11 +1,61 @@
 import React from "react";
+import { Component } from "react";
+import { Button } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Checkout = props => {
-    return (
-        <div>
-            <p>Checkout</p>
-        </div>
-    )
+class Checkout extends Component {
+    state = {
+        values: {
+            deliveryAddress: '',
+            phone: '',
+            paymentType: 'Cash On Delivery',
+        }
+    }
+
+    goBack = () => {
+        this.props.navigate('/');
+    }
+
+    inputChange = e => {
+        this.setState({
+            values: {
+                ...this.state.values,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    submit = () => {
+        console.log(this.state.values)
+    }
+
+    render() {
+        return (
+            <div>
+                <form style={{
+                    border: '1px solid grey',
+                    boxShadow: '1px 1px #888888',
+                    borderRadius: '5px',
+                    padding: '20px'
+                }}>
+                    <textarea name="deliveryAddress" value={this.state.values.deliveryAddress} className="form-control" placeholder="Your Address" onChange={(e)=> this.inputChange(e)} /><br />
+                    <input name="phone" className="form-control" value={this.state.values.phone} placeholder="Your Phone Number" onChange={(e)=> this.inputChange(e)}/><br />
+                    <select name='paymentType' className="form-control" value={this.state.paymentType} onChange={(e)=> this.inputChange(e)}>
+                        <option value='Cash On Delivery'>Cash On Delivery</option>
+                        <option value='Bkash'>Bkash</option>
+                    </select><br />
+                    <Button style={{ backgroundColor: '#d70f64' }} className="me-auto" onClick={this.submit}>Place Order</Button>
+                    <Button color="secondary" className="ms-1" onClick={this.goBack}>Cancel</Button>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default Checkout;
+function WithNavigate(props) {
+    let navigate = useNavigate();
+    return <Checkout {...props} navigate = { navigate } />
+}
+
+export default WithNavigate;
